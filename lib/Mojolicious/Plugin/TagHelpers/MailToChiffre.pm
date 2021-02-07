ce1ede72390ce32507ff49e56baf20e9648388bc
Akron <nils@diewald-online.de>
2021-02-07 17:00:49 +0100

    Move bugtracker to GitHub

--- lib/Mojolicious/Plugin/TagHelpers/MailToChiffre.pm
+++ lib/Mojolicious/Plugin/TagHelpers/MailToChiffre.pm
@@ -726,7 +726,7 @@ L<Mojolicious::Plugin::Util::RandomString>.
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2014-2020, Nils Diewald.
+Copyright (C) 2014-2021, L<Nils Diewald|https://www.nils-diewald.de/>.
 
 This program is free software, you can redistribute it
 and/or modify it under the same terms as Perl.
